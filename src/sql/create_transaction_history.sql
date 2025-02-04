acd077eb895e69aef8dca6ace7eaed2921c90a96
FAL <falworks.contact@gmail.com>
2025-02-04 22:31:12 +0900

    add default constraints

--- src/sql/create_transaction_history.sql
+++ src/sql/create_transaction_history.sql
@@ -1,10 +1,10 @@
 CREATE TABLE IF NOT EXISTS transaction_history (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
   date TEXT NOT NULL,
-  description TEXT NOT NULL,
+  description TEXT DEFAULT '',
   amount INTEGER NOT NULL,
-  major_category TEXT NOT NULL,
-  minor_category TEXT NOT NULL,
-  memo TEXT,
+  major_category TEXT DEFAULT 'uncategorized',
+  minor_category TEXT DEFAULT 'uncategorized',
+  memo TEXT DEFAULT '',
   mf_transaction_id INTEGER UNIQUE -- FK
 );
