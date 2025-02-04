ce90d520e033d29c5a388f4eeaefd6049feaa909
FAL <falworks.contact@gmail.com>
2025-02-05 04:10:34 +0900

    NOT NULL 制約を追加

--- src/sql/create_transaction_history.sql
+++ src/sql/create_transaction_history.sql
@@ -1,10 +1,10 @@
 CREATE TABLE IF NOT EXISTS transaction_history (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
   date TEXT NOT NULL,
-  description TEXT DEFAULT '',
+  description TEXT NOT NULL DEFAULT '',
   amount INTEGER NOT NULL,
-  major_category TEXT DEFAULT 'uncategorized',
-  minor_category TEXT DEFAULT 'uncategorized',
-  memo TEXT DEFAULT '',
+  major_category TEXT NOT NULL DEFAULT 'uncategorized',
+  minor_category TEXT NOT NULL DEFAULT 'uncategorized',
+  memo TEXT NOT NULL DEFAULT '',
   mf_transaction_id INTEGER UNIQUE -- FK
 );
