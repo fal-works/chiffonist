62c5da550e659046f9ea0527dab1cd1a421ca22c
FAL <falworks.contact@gmail.com>
2025-02-10 21:56:23 +0900

    print モジュールで utils を利用

--- src/db/print.rs
+++ src/db/print.rs
@@ -1,4 +1,5 @@
 use crate::db::error::DbError;
+use crate::db::utils;
 
 pub fn print_mf_transaction_summary() -> Result<(), DbError> {
     let conn = rusqlite::Connection::open("data/transactions.db")?;
@@ -6,43 +7,23 @@ pub fn print_mf_transaction_summary() -> Result<(), DbError> {
     let count: i64 = conn.query_row("SELECT COUNT(*) FROM mf_transaction", [], |row| row.get(0))?;
     println!("mf_transaction total records: {}", count);
 
-    let mut stmt = conn.prepare("SELECT * FROM mf_transaction LIMIT 10")?;
-    let rows = stmt.query_map([], |row| {
-        Ok((
-            row.get::<_, i32>(0)?,
-            row.get::<_, i32>(1)?,
-            row.get::<_, String>(2)?,
-            row.get::<_, String>(3)?,
-            row.get::<_, i32>(4)?,
-            row.get::<_, String>(5)?,
-            row.get::<_, String>(6)?,
-            row.get::<_, String>(7)?,
-            row.get::<_, String>(8)?,
-            row.get::<_, i32>(9)?,
-            row.get::<_, String>(10)?,
-        ))
-    })?;
-
     println!("mf_transaction first 10 records:");
-    for record in rows {
-        let (
-            id,
-            include_flag,
-            occurrence_date,
-            particulars,
-            amount,
-            financial_institution,
-            major_category,
-            intermediate_category,
-            memo,
-            transfer_flag,
-            mf_original_id,
-        ) = record?;
-        println!(
-          "ID: {}, 計算対象: {}, 日付: {}, 内容: {}, 金額: {}, 金融機関: {}, 大項目: {}, 中項目: {}, メモ: {}, 振替: {}, MF ID: {}",
-          id, include_flag, occurrence_date, particulars, amount, financial_institution, major_category, intermediate_category, memo, transfer_flag, mf_original_id
-      );
-    }
+    let mut stmt: rusqlite::Statement<'_> =
+        conn.prepare("SELECT * FROM mf_transaction LIMIT 10;")?;
+    let column_names = [
+        "ID",
+        "計算対象",
+        "日付",
+        "内容",
+        "金額",
+        "金融機関",
+        "大項目",
+        "中項目",
+        "メモ",
+        "振替",
+        // "MF ID",
+    ];
+    utils::print_select_query(&mut stmt, &column_names)?;
 
     Ok(())
 }
@@ -55,29 +36,20 @@ pub fn print_transaction_summary() -> Result<(), DbError> {
     })?;
     println!("transaction_history total records: {}", count);
 
-    let mut stmt = conn.prepare("SELECT * FROM transaction_history LIMIT 10")?;
-    let rows = stmt.query_map([], |row| {
-        Ok((
-            row.get::<_, i32>(0)?,
-            row.get::<_, String>(1)?,
-            row.get::<_, String>(2)?,
-            row.get::<_, i32>(3)?,
-            row.get::<_, String>(4)?,
-            row.get::<_, String>(5)?,
-            row.get::<_, String>(6)?,
-            row.get::<_, String>(7)?,
-        ))
-    })?;
-
     println!("transaction_history first 10 records:");
-    for record in rows {
-        let (id, occurrence_date, particulars, amount, channel, category, sub_category, memo) =
-            record?;
-        println!(
-            "ID: {}, Date: {}, Particulars: {}, Amount: {}, Channel: {}, Category: {}, Sub-category: {}, Memo: {}",
-            id, occurrence_date, particulars, amount, channel, category, sub_category, memo
-        );
-    }
+    let mut stmt: rusqlite::Statement<'_> =
+        conn.prepare("SELECT * FROM transaction_history LIMIT 10;")?;
+    let column_names = [
+        "ID",
+        "日付",
+        "摘要",
+        "金額",
+        "チャネル",
+        "カテゴリー",
+        "サブカテゴリー",
+        "メモ",
+    ];
+    utils::print_select_query(&mut stmt, &column_names)?;
 
     Ok(())
 }
