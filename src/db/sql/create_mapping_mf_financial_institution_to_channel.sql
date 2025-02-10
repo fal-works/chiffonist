2a6e6063dce028f968d50acd4a64ae8d92008d05
FAL <falworks.contact@gmail.com>
2025-02-11 01:20:42 +0900

    NOT NULL 制約の不足を修正

--- src/db/sql/create_mapping_mf_financial_institution_to_channel.sql
+++ src/db/sql/create_mapping_mf_financial_institution_to_channel.sql
@@ -1,4 +1,4 @@
 CREATE TABLE IF NOT EXISTS mapping_mf_financial_institution_to_channel (
-  mf_financial_institution TEXT PRIMARY KEY,
+  mf_financial_institution TEXT NOT NULL PRIMARY KEY,
   channel TEXT NOT NULL
 );
--- src/db/sql/create_tmp_categorized_mf_transaction.sql
+++ src/db/sql/create_tmp_categorized_mf_transaction.sql
@@ -1,5 +1,5 @@
 CREATE TEMPORARY TABLE categorized_mf_transaction (
-  id INTEGER UNIQUE,
+  id INTEGER NOT NULL UNIQUE,
   channel TEXT NOT NULL,
   category TEXT NOT NULL,
   sub_category TEXT NOT NULL
